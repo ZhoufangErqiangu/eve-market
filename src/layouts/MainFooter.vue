89a4cdefd2af80b4e817692689a3451606883ace
alex <liuhlightning@qq.com>
2024-07-12 12:05:10 +0800

    feat foot

--- src/layouts/MainFooter.vue
+++ src/layouts/MainFooter.vue
@@ -1,11 +1,33 @@
 <template>
-  <p class="text1">
-    Power by ZhoufangErqiangu
-  </p>
+  <div class="footer-box">
+    <el-space alignment="middle">
+      <p class="text1">
+        Powered by ZhoufangErqiangu
+      </p>
+      <p>
+        <a href="https://github.com/ZhoufangErqiangu/eve-market" target="_blank">
+          <img class="icon" src="/img/github.svg" alt="GitHub">
+        </a>
+      </p>
+    </el-space>
+  </div>
 </template>
 
+<script lang="ts" setup>
+import { ElSpace } from "element-plus";
+import "element-plus/es/components/space/style/css";
+</script>
+
 <style lang="less" scoped>
+.footer-box {
+  text-align: center;
+}
+
 .text1 {
   text-align: center;
+}
+
+.icon {
+  object-fit: contain;
 }
 </style>
