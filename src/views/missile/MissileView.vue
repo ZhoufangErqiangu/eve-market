49161ce8138e1c087e9c5af9f781b16ed8e9c561
alex <liuhlightning@qq.com>
2024-07-27 11:36:07 +0800

    feat drf

--- src/views/missile/MissileView.vue
+++ src/views/missile/MissileView.vue
@@ -3,8 +3,11 @@
   <el-row :gutter="25">
     <el-col :span="12">
       <el-form :model="form" :rules="rules" label-width="180px">
+        <el-form-item :label="$t('Missile drf')" prop="damageReductionFactor">
+          <el-input-number v-model="form.damageReductionFactor" :min="0" :step="0.01" />
+        </el-form-item>
         <el-form-item :label="$t('Missile damage')" prop="damage">
-          <el-input-number v-model="form.damage" :min="0" :step="0.01" />
+          <el-input-number v-model="form.damage" :min="0" :step="1" />
         </el-form-item>
         <el-form-item :label="$t('Fire interval')" prop="fireInterval">
           <el-input-number v-model="form.fireInterval" :min="0.01" :step="0.01" />
@@ -13,14 +16,14 @@
           <el-input-number v-model="form.explosionRadius" :min="1" :step="0.01" />
         </el-form-item>
         <el-form-item :label="$t('Explosion velocity')" prop="explosionVelocity">
-          <el-input-number v-model="form.explosionVelocity" :min="1" :step="0.01" />
+          <el-input-number v-model="form.explosionVelocity" :min="1" :step="1" />
         </el-form-item>
         <el-divider />
         <el-form-item :label="$t('Target signal radius')" prop="signalRadius">
           <el-input-number v-model="form.signalRadius" :min="1" :step="0.01" />
         </el-form-item>
         <el-form-item :label="$t('Target velocity')" prop="velocity">
-          <el-input-number v-model="form.velocity" :min="1" :step="0.01" />
+          <el-input-number v-model="form.velocity" :min="1" :step="1" />
         </el-form-item>
         <el-divider />
         <el-form-item>
@@ -44,20 +47,21 @@
 </template>
 
 <script lang="ts" setup>
-import { ElForm, ElFormItem, ElInputNumber, type FormRules, ElDivider, ElRow, ElCol, ElTable, ElTableColumn, ElButton } from "element-plus";
+import { ElButton, ElCol, ElDivider, ElForm, ElFormItem, ElInputNumber, ElMessage, ElRow, ElTable, ElTableColumn, type FormRules } from "element-plus";
+import "element-plus/es/components/button/style/css";
+import "element-plus/es/components/col/style/css";
+import "element-plus/es/components/divider/style/css";
 import "element-plus/es/components/form-item/style/css";
 import "element-plus/es/components/form/style/css";
 import "element-plus/es/components/input-number/style/css";
-import "element-plus/es/components/divider/style/css";
 import "element-plus/es/components/row/style/css";
-import "element-plus/es/components/col/style/css";
-import "element-plus/es/components/table/style/css";
 import "element-plus/es/components/table-column/style/css";
-import "element-plus/es/components/button/style/css";
+import "element-plus/es/components/table/style/css";
 import { computed, onMounted, ref } from "vue";
 
 const form = ref<{
   // attack
+  damageReductionFactor?: number;
   damage?: number;
   fireInterval?: number;
   explosionRadius?: number;
@@ -67,6 +71,10 @@ const form = ref<{
   signalRadius?: number;
 }>({});
 const rules: FormRules = {
+  damageReductionFactor: [
+    { required: true, message: "Please input damage reduction factor", trigger: "blur" },
+    { type: "number", message: "Please input a number", trigger: "blur" },
+  ],
   damage: [
     { required: true, message: "Please input damage", trigger: "blur" },
     { type: "number", message: "Please input a number", trigger: "blur" },
@@ -94,8 +102,8 @@ const rules: FormRules = {
 };
 
 const result = computed(() => {
-  const { damage, fireInterval, explosionRadius, explosionVelocity, velocity, signalRadius } = form.value;
-  if (damage && fireInterval && explosionRadius && explosionVelocity && velocity && signalRadius) {
+  const { damageReductionFactor, damage, fireInterval, explosionRadius, explosionVelocity, velocity, signalRadius } = form.value;
+  if (damageReductionFactor && damage && fireInterval && explosionRadius && explosionVelocity && velocity && signalRadius) {
     // on board
     const dpsOnBoard = damage / fireInterval;
     // real
@@ -104,7 +112,7 @@ const result = computed(() => {
       Math.min(
         1,
         signalRadius / explosionRadius,
-        (signalRadius * explosionVelocity) / (explosionRadius * velocity),
+        ((signalRadius * explosionVelocity) / (explosionRadius * velocity)) ** damageReductionFactor,
       ));
     const dph = damage * magnification;
     const dps = dph / fireInterval;
@@ -128,6 +136,7 @@ const result = computed(() => {
 const STORAGE_KEY = "missile";
 function onSave() {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(form.value));
+  ElMessage.success("Saved");
 }
 onMounted(() => {
   const data = localStorage.getItem(STORAGE_KEY);
